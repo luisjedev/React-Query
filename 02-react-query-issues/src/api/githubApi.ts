0b4ca498a8a3501c6793734385c0add0c7ed5f67
Luis Gómez <55402700+luisjedev@users.noreply.github.com>
2024-02-22 09:51:40 +0100

    feat: :sparkles: Github token
    Obtener y incluir el token de github para las peticiones

--- 02-react-query-issues/src/api/githubApi.ts
+++ 02-react-query-issues/src/api/githubApi.ts
@@ -1,8 +1,9 @@
-import axios from 'axios';
-
+import axios from "axios";
 
 export const githubApi = axios.create({
-    baseURL: 'https://api.github.com/repos/facebook/react',
-    headers: {}
+  baseURL: "https://api.github.com/repos/facebook/react",
+  headers: {
+    Authorization:
+      "Bearer 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",
+  },
 });
-
