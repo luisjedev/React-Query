de3e5a0059745df982b74d61f4a4b24096ebc68d
Luis Gómez <55402700+luisjedev@users.noreply.github.com>
2024-02-23 09:52:29 +0100

    renovar apikey

--- 02-react-query-issues/src/api/githubApi.ts
+++ 02-react-query-issues/src/api/githubApi.ts
@@ -4,6 +4,6 @@ export const githubApi = axios.create({
   baseURL: "https://api.github.com/repos/facebook/react",
   headers: {
     Authorization:
-      "Bearer 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",
+      "Bearer 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",
   },
 });
